c6abd0ba99502bdf7cfed924d2af846330bd4bb3
JonasMoura-J <jonasmourat9090@gmail.com>
2020-11-21 18:42:52 -0300

    Implementando classes DaoFactory e VendedorDaoJDBC

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.VendedorDao;
 import model.entities.Departamento;
 import model.entities.Vendedor;
 
@@ -10,6 +12,9 @@ public class Main {
 	public static void main(String[] args) {
 		Departamento obj = new Departamento(1, "Livros");
 		Vendedor vendedor = new Vendedor(21, "Jonas", "jonas@gmail.com", new Date(), 3000.0, obj);
+		
+		VendedorDao vd = DaoFactory.createVendedorDao();
+		
 		System.out.println(vendedor);
 	}
 
